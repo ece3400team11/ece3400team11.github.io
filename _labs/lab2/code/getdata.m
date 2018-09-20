c073e6885a680d427a0b328e0509a4d4a53128bd
Daniel Weber <dantech88@yahoo.com>
2018-09-19 20:54:22 -0400

    added lab2 code

---
+++ _labs/lab2/code/getdata.m
@@ -0,0 +1,16 @@
+l = seriallist;
+l(3)
+myserialport = serial("/dev/cu.wchusbserial1410", "BaudRate", 9600)
+fopen(myserialport)
+binNums = [0:127];
+bins = zeros(1,128);
+try
+    start = fscanf(myserialport,"%s");
+    for i = 1:128
+        bins(i) = fscanf(myserialport,"%d")
+    end
+catch ME
+    warning('error occured');
+end
+fclose(myserialport)
+plot(binNums, bins)
---
+++ _labs/lab2/code/testFFT/testFFT.ino
@@ -0,0 +1,46 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  TIMSK0 = 0; // turn off timer0 for lower jitter - delay() and millis() killed
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(1) { // reduces jitter
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }// window data, then reorder, then run, then take output
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei(); // turn interrupts back on
+    int sum = 0;
+    for(int i = 41; i < 44; i++) {
+      sum += fft_log_out[i];
+    }
+    // 200 cuttoff
+    if (sum > 170) {
+      Serial.println("robot");  
+    } else {
+      Serial.println("nobody");  
+    }
+  }
+}
